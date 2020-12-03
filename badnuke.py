6e27655893d607767c00b088b4aebd0e9d17c75a
hijackediscool <75279793+hijackediscool@users.noreply.github.com>
2020-12-03 00:20:37 +0000

    Create badnuke.py

---
+++ badnuke.py
@@ -0,0 +1,46 @@
+token = ""
+#have intents on#
+
+
+prefix = ""
+
+import discord, random, aiohttp, asyncio
+from discord.ext import commands
+from discord import Webhook, AsyncWebhookAdapter
+from discord.ext.commands import *
+from colorama import Fore as C
+from colorama import Style as S
+
+
+
+bot = commands.Bot(command_prefix = prefix, intents = discord.Intents.all())
+
+
+
+
+
+@bot.event
+async def on_ready():
+  print(f"""bot is ready""")
+
+
+
+
+
+
+@bot.command()
+async def spam(ctx, *, message):
+  await ctx.message.delete()
+  while True:
+    await ctx.send(message)
+
+@bot.command()
+async def lol(ctx):
+    for user in ctx.guild.members:
+        await user.ban(reason = "haha server go boom")
+
+
+#dont touch anything below this#
+
+
+bot.run(token)
